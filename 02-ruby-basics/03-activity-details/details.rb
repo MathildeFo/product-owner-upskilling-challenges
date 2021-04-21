9a7be0d7708530c2598f6fcaca72c5b3203f508a
Mathilde FOSSAT <mathilde.fossat@polytechnique.edu>
2021-04-21 14:34:53 +0000

    price

--- 02-ruby-basics/03-activity-details/details.rb
+++ 02-ruby-basics/03-activity-details/details.rb
@@ -4,11 +4,11 @@ activity = {
   "City" => "Nantes",
   "Duration(min)" => 60,
   "Number of participants" => 4,
-  "Price(euros)" => 120
+  "Price(euros)" => 123
 }
 
 puts "#{activity["Name"]} [#{activity["Category"]}]"
 puts "Located in #{activity["City"]}"
 puts "Duration of #{activity["Duration(min)"]}minutes"
 puts "For #{activity["Number of participants"]} participants"
-puts "Carged #{activity["Price(euros)"]}euros"
+puts "Charged #{activity["Price(euros)"]}euros"
