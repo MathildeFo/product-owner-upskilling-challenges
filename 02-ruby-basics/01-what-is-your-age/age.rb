aabf5592b409233939d04036748f8f541f4fec2b
Mathilde FOSSAT <mfossat@lucca.fr>
2021-04-21 13:33:36 +0000

    age

--- 02-ruby-basics/01-what-is-your-age/age.rb
+++ 02-ruby-basics/01-what-is-your-age/age.rb
@@ -3,4 +3,8 @@ name = gets.chomp # asks user to type something in the Terminal
 
 puts "Ok got it!"
 
-# TODO: your code goes here
+puts "What is your year of birth?"
+year = gets.chomp
+yearinter = year.to_i
+age= 2021-yearinter
+puts "Hey #{name}, Your are so old! You will be #{age} this year."
