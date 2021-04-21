fbb5fd85bfffd3c5bbf1d5041bd4e178c9ffe718
Mathilde FOSSAT <mfossat@lucca.fr>
2021-04-21 14:23:32 +0000

    LISTING

--- 02-ruby-basics/04-listing-activities/listing.rb
+++ 02-ruby-basics/04-listing-activities/listing.rb
@@ -1,5 +1,35 @@
 activities = [
-  # TODO: gather at least 3 activities
+ {
+    "Name" => "Escape game",
+    "Category" => "ADVENTURE",
+    "City" => "Nantes",
+    "Duration(min)" => 60,
+    "Number of participants" => 4,
+    "Price(euros)" => 120
+  },
+  {
+    "Name" => "Skydiving",
+    "Category" => "ADVENTURE",
+    "City" => "Marseille",
+    "Duration(min)" => 30,
+    "Number of participants" => 6,
+    "Price(euros)" => 234
+  },
+  {
+    "Name" => "Boatparty",
+    "Category" => "Food and fun",
+    "City" => "Marseille",
+    "Duration(min)" => 240,
+    "Number of participants" => 20,
+    "Price(euros)" => 800
+  },
 ]
 
-# TODO: your code goes here
+activities.each do |activity|
+puts "#{activity["Name"]} [#{activity["Category"]}]"
+puts "Located in #{activity["City"]}"
+puts "Duration of #{activity["Duration(min)"]}minutes"
+puts "For #{activity["Number of participants"]} participants"
+puts "Charged #{activity["Price(euros)"]}euros"
+puts "---------"
+end
