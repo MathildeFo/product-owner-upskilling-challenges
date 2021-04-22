9dd014943b9bdbd221df4244267e008a887945c1
Mathilde FOSSAT <mathilde.fossat@polytechnique.edu>
2021-04-22 13:11:51 +0000

    ajouts des queries sql

--- 04-databases/02-sql-queries/queries.sql
+++ 04-databases/02-sql-queries/queries.sql
@@ -3,97 +3,71 @@
 ---------------------------------------
 
 -- Get all sites
--- TODO: SELECT ...
+SELECT * from sites;
 
 -- Get all sites sorted by name
--- TODO: SELECT ...
+SELECT * from sites ORDER BY name;
 
 -- Gett all activity names sorted by name
--- TODO: SELECT ...
+SELECT * from activities ORDER BY name;
 
 -- Get all "Sport" activities
--- TODO: SELECT ...
--- Expected result: 8 rows
+SELECT * from activities WHERE category="Sport";
 
 -- Get all Escape Games in Nantes
--- TODO: SELECT ...
--- Expected result: 3 rows
+SELECT * from activities WHERE name like "%Escape%" ADN city="Nantes";
 
 ---------------------------------------
 -- [AGGREGATES]
 ---------------------------------------
 
 -- Count all the activities
--- TODO: SELECT ...
--- Expected result: 44
+SELECT count(*) from activities;
 
 -- Count all "Adventure" activities
--- TODO: SELECT ...
--- Expected result: 13
+SELECT count(*) from activities WHERE category="Adventure";
 
 -- Count the number of activities per category
--- TODO: SELECT ...
--- Expected result:
--- category    total
--- ----------  ----------
--- Adventure   13
--- Cultural    6
--- Food        6
--- Sport       8
--- Visits      5
--- Workshops   6
+SELECT category, count(*) as total from activities group by category;
 
 -- Get the 3 category names with the biggest number of activities
--- TODO: SELECT ...
--- Expected result:
--- category    total
--- ----------  ----------
--- Adventure   13
--- Sport       8
--- Cultural    6
+SELECT category , count(*) as total from activities group by category ORDER by total DESC LIMIT 3;
 
 ---------------------------------------
 -- [JOINS]
 ---------------------------------------
 
 -- Get all employee names working FROM site "Graslin", sorted by first name
--- TODO: SELECT ...
--- Expected result: 53 rows
+SELECT * from employees
+JOIN sites ON sites.id=employees.site_id
+WHERE employees.site_id=2 ORDER by first_name;
 
 -- Get all the activity names where you have been to, sorted by name
--- TODO: SELECT ...
--- Expected result: well, it depends on who you are :)
+SELECT activities.name from participations
+JOIN employees ON employees.id=participations.employee_id
+JOIN team_building_sessions ON team_building_sessions.id=participations.team_building_session_id
+JOIN activities ON activities.id=team_building_sessions.activity_id
+WHERE employees.first_name="Mathilde";
 
 -- [NEW KEYWORD] Get all the team names that have done an Adventure activity, sorted by names
--- TODO: SELECT ...
--- Expected result:
--- name
--- --------------------
--- Business Development
--- Product Owner
--- R&D
+SELECT teams.name from teams
+JOIN team_building_sessions ON team_building_sessions.team_id=teams.id
+JOIN activities ON activities.id=team_building_sessions.activity_id
+WHERE activities.category= "Adventure"
+GROUP BY teams.name;
 
 -- Get the team names and total number of team building sessions done, sorted by top teams
--- TODO: SELECT ...
--- Expected result:
--- name        total_sessions
--- ----------  --------------
--- R&D         7
--- Marketing   4
--- Business D  3
--- Product Ow  3
--- Finance &   2
--- UI / UX     2
--- Customer S  1
+SELECT teams.name, count(*) as total from team_building_sessions
+JOIN teams ON teams.id = team_building_sessions.team_id
+GROUP by teams.name
+ORDER by total DESC;
 
 -- Get all the employee names, team names and site names that have never attended a team building session
--- TODO: SELECT ...
--- Expected result:
--- first_name  last_name   team        site
--- ----------  ----------  ----------  -----------
--- Harmony     Florin      Channels    Paris 13ème
--- Julia       Ivanets     Channels    Paris 13ème
--- Pierre      Pellan      Channels    Paris 13ème
+SELECT first_name, last_name, teams.name, sites.name from employees
+JOIN sites ON sites.id=employees.site_id
+JOIN teams ON teams.id=employees.team_id
+LEFT OUTER JOIN participations ON participations.employee_id=employees.id
+WHERE participations.id IS NULL;
 
 -- [NEW AGGREGATE] Get the budget spent on team building sessions per team, sorted by most expensive to least expensive
 -- TODO: SELECT ...
